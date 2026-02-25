82c0084870fdd4a0dfa097b452495c8cc9499a69
netman <netman@netman-VirtualBox>
2026-02-24 18:17:30 -0700

    update files

--- NMmain.py
+++ NMmain.py
@@ -1,1 +1,20 @@
 import NMdhcp, NMgithub, NMsnmp, NMtcpdump
+
+def main():
+    print("starting tcpdump analysis...")
+    NMtcpdump.main()
+    
+    print("starting dhcp configuration...")
+    NMdhcp.main()
+
+    print("starting snmp monitoring...")
+    NMsnmp.main()
+
+    print("pushing to github...")
+    NMgithub.main()
+
+    print("lab5 complete.")
+
+
+if __name__ == "__main__":
+    main()
